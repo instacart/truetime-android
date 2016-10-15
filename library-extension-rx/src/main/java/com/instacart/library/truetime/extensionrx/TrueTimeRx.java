d71f02b0d3d7a969cc07d9918ca0c13d4dbc3f99
Kaushik Gopal <code@kaush.co>
2016-10-15 00:19:27 -0700

    feat: implement NTP algo for TrueTime

--- library-extension-rx/src/main/java/com/instacart/library/truetime/extensionrx/TrueTimeRx.java
+++ library-extension-rx/src/main/java/com/instacart/library/truetime/extensionrx/TrueTimeRx.java
@@ -1,11 +1,19 @@
 package com.instacart.library.truetime.extensionrx;
 
 import android.content.Context;
+import android.util.Log;
+import com.instacart.library.truetime.SntpClient;
 import com.instacart.library.truetime.TrueTime;
-import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Callable;
 import rx.Observable;
+import rx.Observable.Transformer;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
@@ -42,52 +50,156 @@ public class TrueTimeRx
     }
 
     /**
-     * Initialize the SntpClient
-     * Issue SNTP call via UDP to list of provided hosts
-     * Pick the first successful call and return
-     * Retry failed calls individually
+     * Initialize TrueTime
+     * See {@link #initializeNtp(String)}
+     *
+     * @return accurate NTP Date
      */
     public Observable<Date> initialize(final List<String> ntpHosts) {
+        String ntpPool = "time.apple.com";
+
+        return initializeNtp(ntpPool)//
+              .map(new Func1<long[], Date>() {
+                  @Override
+                  public Date call(long[] longs) {
+                      return now();
+                  }
+              });
+    }
+
+    /**
+     * Initialize TrueTime
+     * A single NTP pool server is provided. Using DNS we resolve that to multiple IP hosts
+     * Against each IP host we issue a UDP call and retrieve the best response using the NTP algorithm
+     *
+     * Use this instead of {@link #initialize(List)} if you wish to also get additional info for
+     * instrumentation/tracking actual NTP response data
+     *
+     * @param ntpPool NTP pool server e.g. time.apple.com, 0.us.pool.ntp.org
+     * @return Observable of detailed long[] containing most important parts of the actual NTP response
+     * See RESPONSE_INDEX_ prefixes in {@link SntpClient} for details
+     */
+    public Observable<long[]> initializeNtp(String ntpPool) {
         return Observable//
-              .from(ntpHosts)//
-              .flatMap(new Func1<String, Observable<Date>>() {
+              .just(ntpPool)//
+              .compose(resolveNtpPoolToIpAddresses())//
+              .flatMap(bestResponseAgainstSingleIp(5))  // get best response from querying the ip 5 times
+              .take(5)                                  // take 5 of the best results
+              .toList()//
+              .map(filterMedianResponse())//
+              .doOnNext(new Action1<long[]>() {
                   @Override
-                  public Observable<Date> call(String ntpHost) {
-                      return Observable//
-                            .just(ntpHost)//
-                            .subscribeOn(Schedulers.io())//
-                            .flatMap(new Func1<String, Observable<Date>>() {
-                                @Override
-                                public Observable<Date> call(String ntpHost) {
-                                    try {
-                                        initialize(ntpHost);
-                                    } catch (IOException e) {
-                                        return Observable.error(e);
-                                    }
-                                    return Observable.just(now());
-                                }
-                            })//
-                            .retry(_retryCount)//
-                            .onErrorReturn(new Func1<Throwable, Date>() {
-                                @Override
-                                public Date call(Throwable throwable) {
-                                    throwable.printStackTrace();
-                                    return null;
-                                }
-                            }).take(1).doOnNext(new Action1<Date>() {
-                                @Override
-                                public void call(Date date) {
-                                    cacheTrueTimeInfo();
-                                }
-                            });
+                  public void call(long[] ntpResponse) {
+                      cacheTrueTimeInfo(ntpResponse);
+                      saveTrueTimeInfoToDisk();
                   }
-              })//
-              .filter(new Func1<Date, Boolean>() {
-                  @Override
-                  public Boolean call(Date date) {
-                      return date != null;
-                  }
-              })//
-              .take(1);
+              });
+    }
+
+    private Transformer<String, String> resolveNtpPoolToIpAddresses() {
+        return new Transformer<String, String>() {
+            @Override
+            public Observable<String> call(Observable<String> ntpPoolObservable) {
+                return ntpPoolObservable//
+                      .observeOn(Schedulers.io())//
+                      .flatMap(new Func1<String, Observable<InetAddress>>() {
+                          @Override
+                          public Observable<InetAddress> call(String ntpPoolAddress) {
+                              try {
+                                  Log.d("kg", "---- resolving ntpHost : " + ntpPoolAddress);
+                                  return Observable.from(InetAddress.getAllByName(ntpPoolAddress));
+                              } catch (UnknownHostException e) {
+                                  return Observable.error(e);
+                              }
+                          }
+                      })//
+                      .map(new Func1<InetAddress, String>() {
+                          @Override
+                          public String call(InetAddress inetAddress) {
+                              Log.d("kg", "---- ntphost [" +
+                                          inetAddress.getHostName() +
+                                          "] : " +
+                                          inetAddress.getHostAddress());
+                              return inetAddress.getHostAddress();
+                          }
+                      });
+            }
+        };
+    }
+
+    private Func1<String, Observable<long[]>> bestResponseAgainstSingleIp(final int repeatCount) {
+        return new Func1<String, Observable<long[]>>() {
+            @Override
+            public Observable<long[]> call(String singleIp) {
+                return Observable.just(singleIp)//
+                      .repeat(repeatCount)//
+                      .flatMap(new Func1<String, Observable<long[]>>() {
+                          @Override
+                          public Observable<long[]> call(final String singleIpHostAddress) {
+                              return Observable//
+                                    .fromCallable(new Callable<long[]>() {
+                                        @Override
+                                        public long[] call() throws Exception {
+                                            Log.d("kg", "---- requestTime from: " + singleIpHostAddress);
+                                            return requestTime(singleIpHostAddress);
+                                        }
+                                    })//
+                                    .subscribeOn(Schedulers.io())//
+                                    .doOnError(new Action1<Throwable>() {
+                                        @Override
+                                        public void call(Throwable throwable) {
+                                            Log.e("kg", "---- Error requesting time", throwable);
+                                        }
+                                    })//
+                                    .retry(_retryCount);
+                          }
+                      })//
+                      .toList()//
+                      .onErrorResumeNext(Observable.<List<long[]>>empty())
+                      .map(filterLeastRoundTripDelay()); // pick best response for each ip
+            }
+        };
+    }
+
+    private Func1<List<long[]>, long[]> filterLeastRoundTripDelay() {
+        return new Func1<List<long[]>, long[]>() {
+            @Override
+            public long[] call(List<long[]> responseTimeList) {
+
+                Log.d("kg", "---- filterLeastRoundTrip: " + responseTimeList);
+
+                Collections.sort(responseTimeList, new Comparator<long[]>() {
+                    @Override
+                    public int compare(long[] lhsParam, long[] rhsLongParam) {
+                        long lhs = SntpClient.getRoundTripDelay(lhsParam);
+                        long rhs = SntpClient.getRoundTripDelay(rhsLongParam);
+                        return lhs < rhs ? -1 : (lhs == rhs ? 0 : 1);
+                    }
+                });
+
+                return responseTimeList.get(0);
+            }
+        };
+    }
+
+    private Func1<List<long[]>, long[]> filterMedianResponse() {
+        return new Func1<List<long[]>, long[]>() {
+            @Override
+            public long[] call(List<long[]> bestResponses) {
+                Collections.sort(bestResponses, new Comparator<long[]>() {
+                    @Override
+                    public int compare(long[] lhsParam, long[] rhsParam) {
+                        long lhs = SntpClient.getClockOffset(lhsParam);
+                        long rhs = SntpClient.getClockOffset(rhsParam);
+                        return lhs < rhs ? -1 : (lhs == rhs ? 0 : 1);
+                    }
+                });
+
+                Log.d("kg", "---- bestResponses: " + bestResponses);
+                Log.d("kg", "---- bestResponse: " + Arrays.toString(bestResponses.get(bestResponses.size() / 2)));
+
+                return bestResponses.get(bestResponses.size() / 2);
+            }
+        };
     }
 }
