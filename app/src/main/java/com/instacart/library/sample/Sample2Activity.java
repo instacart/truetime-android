0b5ab66b02c1fa233446ad3d4b4079464535ce3e
Kaushik Gopal <code@kaush.co>
2017-04-06 11:40:28 -0700

    feat: upgrade to RxJava 2.x

--- app/src/main/java/com/instacart/library/sample/Sample2Activity.java
+++ app/src/main/java/com/instacart/library/sample/Sample2Activity.java
@@ -10,15 +10,16 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.instacart.library.truetime.TrueTimeRx;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action0;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
+
 
 public class Sample2Activity
       extends AppCompatActivity {
@@ -50,19 +51,20 @@ public class Sample2Activity
               .initializeRx("time.google.com")
               .subscribeOn(Schedulers.io())
               .observeOn(AndroidSchedulers.mainThread())
-              .subscribe(new Action1<Date>() {
+              .subscribe(new Consumer<Date>() {
                   @Override
-                  public void call(Date date) {
+                  public void accept(Date date) {
                       onBtnRefresh();
                   }
-              }, new Action1<Throwable>() {
+              }, new Consumer<Throwable>() {
                   @Override
-                  public void call(Throwable throwable) {
-                      Log.e(TAG, "something went wrong when trying to initializeRx TrueTime", throwable);
+                  public void accept(Throwable throwable) {
+                      Log.e(TAG, "something went wrong when trying to initializeRx TrueTime",
+                          throwable);
                   }
-              }, new Action0() {
+              }, new Action() {
                   @Override
-                  public void call() {
+                  public void run() throws Exception {
                       refreshBtn.setEnabled(true);
                   }
               });
