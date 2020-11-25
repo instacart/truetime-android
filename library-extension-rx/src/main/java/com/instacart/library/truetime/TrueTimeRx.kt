843f9cc179499f4393d4fc6a11ee66e82934a2dd
KG <kaushik@instacart.com>
2020-11-25 09:57:22 -0800

    simplify Rx in TrueTimeRx

--- library-extension-rx/src/main/java/com/instacart/library/truetime/TrueTimeRx.kt
+++ library-extension-rx/src/main/java/com/instacart/library/truetime/TrueTimeRx.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.FlowableOnSubscribe
-import io.reactivex.FlowableTransformer
 import io.reactivex.Single
 import io.reactivex.functions.Function
 import io.reactivex.schedulers.Schedulers
@@ -110,8 +109,8 @@ class TrueTimeRx : TrueTime() {
     fun initializeNtp(ntpPool: String): Single<LongArray> {
         return Flowable
             .just(ntpPool)
-            .compose(resolveNtpPoolToIpAddresses())
-            .compose(performNtpAlgorithm())
+            .resolveNtpPoolToIpAddresses()
+            .performNtpAlgorithm()
             .firstOrError()
     }
 
@@ -128,7 +127,7 @@ class TrueTimeRx : TrueTime() {
      */
     fun initializeNtp(resolvedNtpAddresses: List<InetAddress>): Single<LongArray> {
         return Flowable.fromIterable(resolvedNtpAddresses)
-            .compose(performNtpAlgorithm())
+            .performNtpAlgorithm()
             .firstOrError()
     }
 
@@ -136,64 +135,59 @@ class TrueTimeRx : TrueTime() {
      * Transformer that takes in a pool of NTP addresses
      * Against each IP host we issue a UDP call and retrieve the best response using the NTP algorithm
      */
-    private fun performNtpAlgorithm(): FlowableTransformer<InetAddress, LongArray> {
-        return FlowableTransformer { inetAddressObservable ->
-            inetAddressObservable
-                .map { inetAddress -> inetAddress.hostAddress }
-                .flatMap(bestResponseAgainstSingleIp(5))  // get best response from querying the ip 5 times
-                .take(5)                                  // take 5 of the best results
-                .toList()
-                .toFlowable()
-                .filter { longs -> longs.size > 0 }
-                .map(filterMedianResponse())
-                .doOnNext { ntpResponse ->
-                    cacheTrueTimeInfo(ntpResponse)
-                    TrueTime.saveTrueTimeInfoToDisk()
-                }
-        }
+    private fun Flowable<InetAddress>.performNtpAlgorithm(): Flowable<LongArray> {
+        return map { it.hostAddress }
+            .flatMap { host ->
+                // get best response from querying the ip 5 times
+                bestResponseAgainstSingleIp(singleIp = host)
+            }
+            .take(5)      // take max 5 of the best results
+            .toList()
+            .toFlowable()
+            .filter { longs -> longs.size > 0 }
+            .map(filterMedianResponse())
+            .doOnNext { ntpResponse ->
+                cacheTrueTimeInfo(ntpResponse)
+                TrueTime.saveTrueTimeInfoToDisk()
+            }
     }
 
-    private fun resolveNtpPoolToIpAddresses(): FlowableTransformer<String, InetAddress> {
-        return FlowableTransformer { ntpPoolFlowable ->
-            ntpPoolFlowable
-                .observeOn(Schedulers.io())
-                .flatMap(Function<String, Flowable<InetAddress>> { ntpPoolAddress ->
+    private fun Flowable<String>.resolveNtpPoolToIpAddresses(): Flowable<InetAddress> {
+        return observeOn(Schedulers.io())
+            .flatMap(Function<String, Flowable<InetAddress>> { ntpPoolAddress ->
+                try {
+                    TrueLog.d(TAG, "---- resolving ntpHost : $ntpPoolAddress")
+                    return@Function Flowable.fromArray(*InetAddress.getAllByName(ntpPoolAddress))
+                } catch (e: UnknownHostException) {
+                    return@Function Flowable.error(e)
+                }
+            })
+    }
+
+    private fun bestResponseAgainstSingleIp(singleIp: String, repeatCount: Long = 5): Flowable<LongArray> {
+        return Flowable
+            .just(singleIp)
+            .repeat(repeatCount)
+            .flatMap { singleIpHostAddress ->
+                Flowable.create(FlowableOnSubscribe<LongArray> { o ->
+                    TrueLog.d(
+                        TAG,
+                        "---- requestTime from: $singleIpHostAddress"
+                    )
                     try {
-                        TrueLog.d(TAG, "---- resolving ntpHost : $ntpPoolAddress")
-                        return@Function Flowable.fromArray(*InetAddress.getAllByName(ntpPoolAddress))
-                    } catch (e: UnknownHostException) {
-                        return@Function Flowable.error(e)
+                        o.onNext(requestTime(singleIpHostAddress))
+                        o.onComplete()
+                    } catch (e: IOException) {
+                        o.tryOnError(e)
                     }
-                })
-        }
-    }
-
-    private fun bestResponseAgainstSingleIp(repeatCount: Int): Function<String, Flowable<LongArray>> {
-        return Function { singleIp ->
-            Flowable
-                .just(singleIp)
-                .repeat(repeatCount.toLong())
-                .flatMap { singleIpHostAddress ->
-                    Flowable.create(FlowableOnSubscribe<LongArray> { o ->
-                        TrueLog.d(
-                            TAG,
-                            "---- requestTime from: $singleIpHostAddress"
-                        )
-                        try {
-                            o.onNext(requestTime(singleIpHostAddress))
-                            o.onComplete()
-                        } catch (e: IOException) {
-                            o.tryOnError(e)
-                        }
-                    }, BackpressureStrategy.BUFFER)
-                        .subscribeOn(Schedulers.io())
-                        .doOnError { throwable -> TrueLog.e(TAG, "---- Error requesting time", throwable) }
-                        .retry(retryCount.toLong())
-                }
-                .toList()
-                .toFlowable()
-                .map(filterLeastRoundTripDelay()) // pick best response for each ip
-        }
+                }, BackpressureStrategy.BUFFER)
+                    .subscribeOn(Schedulers.io())
+                    .doOnError { throwable -> TrueLog.e(TAG, "---- Error requesting time", throwable) }
+                    .retry(retryCount.toLong())
+            }
+            .toList()
+            .toFlowable()
+            .map(filterLeastRoundTripDelay()) // pick best response for each ip
     }
 
     private fun filterLeastRoundTripDelay(): Function<List<LongArray>, LongArray> {
