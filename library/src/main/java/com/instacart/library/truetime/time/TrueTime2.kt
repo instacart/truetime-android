6036da144c84595d09d76644670858f38ce7a5a8
KG <kaushik@instacart.com>
2020-12-28 19:30:20 -0800

    feat: TrueTime2 add more usable apis

--- library/src/main/java/com/instacart/library/truetime/time/TrueTime2.kt
+++ library/src/main/java/com/instacart/library/truetime/time/TrueTime2.kt
@@ -4,13 +4,34 @@ import java.util.Date
 
 interface TrueTime2 {
 
-    fun initialize(with: TrueTimeParameters = TrueTimeParameters())
-
-    fun now(): Date
+    suspend fun initialize(with: TrueTimeParameters = TrueTimeParameters()): Date
 
     /**
-     * return [now] if TrueTime is available otherwise
-     * fallback to System clock date
+     * Was [TrueTime2] initialized by calling [initialize]
+     */
+    fun initialized(): Boolean
+
+    /**
+     * return the [Date] now if [TrueTime2] initialized
+     * otherwise [initialize] first, then return the time
+     */
+    suspend fun now(with: TrueTimeParameters? = null): Date  {
+        return if (initialized()) {
+            nowForced()
+        } else {
+            initialize(with ?: TrueTimeParameters())
+        }
+    }
+
+    /**
+     * return [nowForced] if TrueTime is available otherwise fallback to System clock date
      */
     fun nowSafely(): Date
+
+    /**
+     * return the current time as calculated by TrueTime.
+     * If not initialized, will throw [IllegalStateException]
+     */
+    @Throws(IllegalStateException::class)
+    fun nowForced(): Date
 }
--- library/src/main/java/com/instacart/library/truetime/time/TrueTimeImpl.kt
+++ library/src/main/java/com/instacart/library/truetime/time/TrueTimeImpl.kt
@@ -1,10 +1,9 @@
 package com.instacart.library.truetime.time
 
+import android.os.SystemClock
 import com.instacart.library.truetime.legacy.TrueLog
 import com.instacart.library.truetime.sntp.Sntp
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.InetAddress
 import java.util.Date
@@ -16,35 +15,42 @@ class TrueTimeImpl(private val sntpClient: Sntp) : TrueTime2 {
         private const val TAG: String = "XXX-TrueTime"
     }
 
-    private val coroutineScope = CoroutineScope(Dispatchers.IO)
     private var ttResult: AtomicReference<TrueTimeResult> = AtomicReference()
 
-    override fun initialize(with: TrueTimeParameters) {
-        coroutineScope.launch {
-            val ntpResult = init(with)
-            val now = Date(sntpClient.sntpTime(ntpResult))
-            ttResult.set(TrueTimeResult(ntpResult, now))
-        }
-    }
+    override fun initialized(): Boolean = ttResult.get() != null
 
-    override fun now(): Date {
-        if (ttResult.get() == null) throw IllegalStateException("TrueTime was not initialized successfully yet")
-        return ttResult.get().ntpTimeResult.let { Date(sntpClient.sntpTime(it)) }
+    override suspend fun initialize(with: TrueTimeParameters): Date = withContext(Dispatchers.IO) {
+        val ntpResult = init(with)
+        val now = Date(sntpClient.sntpTime(ntpResult))
+
+        ttResult.set(TrueTimeResult(ntpTimeResult = ntpResult, initializeTime = now))
+        now
     }
 
     override fun nowSafely(): Date {
         return if (ttResult.get() != null) {
-            now()
+            nowForced()
         } else {
             TrueLog.d(TAG, "TrueTime not yet initialized")
             Date()
         }
     }
 
+    override fun nowForced(): Date {
+        if (!initialized()) throw IllegalStateException("TrueTime was not initialized successfully yet")
+
+        val ntpTimeResult = ttResult.get().ntpTimeResult
+        val savedSntpTime: Long = sntpClient.sntpTime(ntpTimeResult)
+        val savedDeviceTime: Long = sntpClient.deviceTime(ntpTimeResult)
+        val currentDeviceTime: Long = SystemClock.elapsedRealtime()
+
+        return Date(savedSntpTime + (currentDeviceTime - savedDeviceTime))
+    }
+
     /**
      * Initialize TrueTime with an ntp pool server address
      */
-    private suspend fun init(with: TrueTimeParameters): LongArray {
+    private fun init(with: TrueTimeParameters): LongArray {
 
         // resolve NTP pool -> single IPs
         return resolveNtpHostToIPs(with.ntpHostPool)
@@ -67,42 +73,43 @@ class TrueTimeImpl(private val sntpClient: Sntp) : TrueTime2 {
     /**
      * resolve ntp host pool address to single IPs
      */
-    @Suppress("BlockingMethodInNonBlockingContext")
-    private suspend fun resolveNtpHostToIPs(ntpHost: String): List<String> = withContext(Dispatchers.IO) {
+    private fun resolveNtpHostToIPs(ntpHost: String): List<String> {
         TrueLog.d(TAG, "---- resolving ntpHost : $ntpHost")
-        InetAddress.getAllByName(ntpHost).map { it.hostAddress }
+        return InetAddress.getAllByName(ntpHost).map { it.hostAddress }
     }
 
-    private suspend fun requestTime(
+    private fun requestTime(
         with: TrueTimeParameters,
         ipHostAddress: String,
-    ): LongArray = withContext(Dispatchers.IO) {
+    ): LongArray {
         // retrying upto 50 times if necessary
         repeat(50 - 1) {
             try {
                 // request Time
-                return@withContext sntpClient.requestTime(with, ipHostAddress)
+                return sntpClient.requestTime(with, ipHostAddress)
             } catch (e: Exception) {
                 TrueLog.e(TAG, "---- Error requesting time", e)
             }
         }
 
         // last attempt
-        sntpClient.requestTime(with, ipHostAddress)
+        return sntpClient.requestTime(with, ipHostAddress)
     }
 
     private fun List<LongArray>.filterLeastRoundTripDelay(): LongArray {
-        return minByOrNull { sntpClient.getRoundTripDelay(it) }
+        return minByOrNull { sntpClient.roundTripDelay(it) }
             ?: throw IllegalStateException("Could not find any results from requestingTime")
     }
 
     private fun List<LongArray>.filterMedianClockOffset(): LongArray {
-        val sortedList = this.sortedBy { sntpClient.getClockOffset(it) }
+        val sortedList = this.sortedBy { sntpClient.clockOffset(it) }
         return sortedList[sortedList.size / 2]
     }
 
     private data class TrueTimeResult(
+
         val ntpTimeResult: LongArray,
+
         val initializeTime: Date,
     )
 }
